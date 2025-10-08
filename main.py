3ac3c27e806a5649b00b5508fe852d3a0f60e0a8
ibaragozz <baragozz1@gmail.com>
2025-10-08 15:59:13 +0300

    all stops seem good but B/W

--- main.py
+++ main.py
@@ -10,21 +10,34 @@ from kivy.uix.label import Label
 import json
 
 
-class RouteButton(MDBoxLayout):
-    def __init__(self, route, minutes, color, **kwargs):
+class RouteInfo(MDBoxLayout):
+    def __init__(self, route_number, next_time, **kwargs):
         super().__init__(**kwargs)
-        self.orientation = "horizontal"
+        self.orientation = "vertical"
         self.size_hint = (None, None)
-        self.size = ("80dp", "40dp")
-        self.md_bg_color = color
-        self.radius = [10]
+        self.size = ("70dp", "50dp")
+        self.spacing = "2dp"
+        self.padding = "5dp"
+        self.radius = [8]
+        self.md_bg_color = (0.95, 0.95, 0.95, 1)  # Светло-серый фон
 
-        # Простой текст
+        # Номер маршрута (крупный текст) - используем обычный Label
         self.add_widget(Label(
-            text=f"{route} ({minutes} мин)",
-            color=(1, 1, 1, 1),
+            text=str(route_number),
+            font_size='18sp',
             bold=True,
-            size_hint=(1, 1)
+            color=(0, 0, 0, 1),  # Черный цвет
+            halign="center",
+            size_hint_y=0.6
+        ))
+
+        # Время следующего автобуса (мелкий текст) - используем обычный Label
+        self.add_widget(Label(
+            text=next_time,
+            font_size='12sp',
+            color=(0.3, 0.3, 0.3, 1),  # Темно-серый цвет
+            halign="center",
+            size_hint_y=0.4
         ))
 
 
@@ -33,25 +46,36 @@ class StopCard(MDCard):
         super().__init__(**kwargs)
         self.orientation = "vertical"
         self.size_hint_y = None
-        self.height = 120
+        self.height = 140
         self.padding = "10dp"
-        self.spacing = "5dp"
+        self.spacing = "10dp"
         self.radius = [15]
 
-        # Заголовок остановки - используем правильный стиль
-        self.add_widget(MDLabel(
+        # Заголовок остановки - используем обычный Label
+        self.add_widget(Label(
             text=stop_name,
-            font_style="Title",  # Используем "Title" вместо "H6"
+            font_size='16sp',
+            bold=True,
+            color=(0, 0, 0, 1),
             size_hint_y=None,
             height=30
         ))
 
-        # Кнопки маршрутов
-        btn_row = MDBoxLayout(orientation="horizontal", spacing="5dp", size_hint_y=None, height=40)
-        for route, minutes, color in routes:
-            btn = RouteButton(route, minutes, color)
-            btn_row.add_widget(btn)
-        self.add_widget(btn_row)
+        # Контейнер для маршрутов
+        routes_container = MDBoxLayout(
+            orientation="horizontal",
+            spacing="10dp",
+            size_hint_y=None,
+            height=60
+        )
+
+        # Добавляем информацию о маршрутах
+        for route_info in routes:
+            route_number, next_time = route_info
+            route_widget = RouteInfo(route_number, next_time)
+            routes_container.add_widget(route_widget)
+
+        self.add_widget(routes_container)
 
 
 class AllStopsScreen(Screen):
@@ -74,10 +98,11 @@ class AllStopsScreen(Screen):
     def update_stops(self):
         self.stops_box.clear_widgets()
 
-        # Тестовые данные
+        # Тестовые данные: (номер_маршрута, время_следующего)
         stops_data = [
-            ("Остановка 1", [("12", 5, (0.8, 0.3, 0.3, 1)), ("25", 12, (0.3, 0.8, 0.3, 1))]),
-            ("Остановка 2", [("7", 3, (0.3, 0.3, 0.8, 1))]),
+            ("Остановка Центральная", [("12", "15:30"), ("25", "15:42"), ("7", "15:55")]),
+            ("Остановка Северная", [("3", "16:10"), ("18", "16:25")]),
+            ("Остановка Южная", [("5", "15:38"), ("12", "15:45"), ("22", "16:00"), ("31", "16:15")]),
         ]
 
         for stop_name, routes in stops_data:
@@ -104,18 +129,22 @@ class FavoritesScreen(Screen):
 
     def update_favorites(self):
         self.favorites_box.clear_widgets()
-        self.favorites_box.add_widget(MDLabel(
+        # Используем обычный Label вместо MDLabel
+        self.favorites_box.add_widget(Label(
             text="Здесь будут избранные остановки",
-            halign="center"
+            halign="center",
+            color=(0.5, 0.5, 0.5, 1)
         ))
 
 
 class AboutScreen(Screen):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.add_widget(MDLabel(
+        # Используем обычный Label вместо MDLabel
+        self.add_widget(Label(
             text="Расписание автобусов\nРазработчик: Вы",
-            halign="center"
+            halign="center",
+            font_size='16sp'
         ))
 
 
@@ -127,15 +156,18 @@ class BusApp(MDApp):
         # Создаем ScreenManager
         screen_manager = ScreenManager()
 
-        # Создаем экраны
-        stops_screen = AllStopsScreen(name="stops")
+        # Создаем экраны - избранное первым
         favorites_screen = FavoritesScreen(name="favorites")
+        stops_screen = AllStopsScreen(name="stops")
         about_screen = AboutScreen(name="about")
 
         # Добавляем экраны в менеджер
         screen_manager.add_widget(favorites_screen)
         screen_manager.add_widget(stops_screen)
         screen_manager.add_widget(about_screen)
+
+        # Устанавливаем стартовый экран - Избранное
+        screen_manager.current = "favorites"
 
         # Создаем основной layout
         main_layout = MDBoxLayout(orientation="vertical")
@@ -150,7 +182,7 @@ class BusApp(MDApp):
             padding="5dp"
         )
 
-        # Кнопки навигации с иконками
+        # Кнопки навигации с иконками - избранное первое
         favorites_btn = MDIconButton(
             icon="star",
             on_release=lambda x: setattr(screen_manager, 'current', 'favorites')
