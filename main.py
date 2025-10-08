5e524e0b22529ddc4f2f38ef7e91d61d4b83e5a2
ibaragozz <baragozz1@gmail.com>
2025-10-08 15:46:18 +0300

    it starts working!change all_stops screen

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
-from kivy.lang import Builder
 from kivy.clock import Clock
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivymd.app import MDApp
 from kivymd.uix.card import MDCard
 from kivymd.uix.label import MDLabel
-from kivymd.uix.button import MDButton, MDIconButton
-from kivy.uix.boxlayout import BoxLayout
-from kivymd.uix.behaviors import RoundedRectangularElevationBehavior
+from kivymd.uix.button import MDIconButton
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivymd.uix.scrollview import MDScrollView
+from kivy.uix.label import Label
 import json
 
 
-class RouteButton(MDButton):
+class RouteButton(MDBoxLayout):
     def __init__(self, route, minutes, color, **kwargs):
         super().__init__(**kwargs)
-        self.text = f"{route} ({minutes} мин)"
-        self.md_bg_color = color
-        self.theme_text_color = "Custom"
-        self.text_color = (1, 1, 1, 1)
+        self.orientation = "horizontal"
         self.size_hint = (None, None)
         self.size = ("80dp", "40dp")
+        self.md_bg_color = color
+        self.radius = [10]
+
+        # Простой текст
+        self.add_widget(Label(
+            text=f"{route} ({minutes} мин)",
+            color=(1, 1, 1, 1),
+            bold=True,
+            size_hint=(1, 1)
+        ))
 
 
 class StopCard(MDCard):
@@ -27,20 +34,20 @@ class StopCard(MDCard):
         self.orientation = "vertical"
         self.size_hint_y = None
         self.height = 120
-        self.padding = 10
-        self.spacing = 5
+        self.padding = "10dp"
+        self.spacing = "5dp"
+        self.radius = [15]
 
-        # Заголовок остановки
+        # Заголовок остановки - используем правильный стиль
         self.add_widget(MDLabel(
             text=stop_name,
-            font_style="H6",
-            theme_text_color="Primary",
+            font_style="Title",  # Используем "Title" вместо "H6"
             size_hint_y=None,
             height=30
         ))
 
         # Кнопки маршрутов
-        btn_row = BoxLayout(orientation="horizontal", spacing=5, size_hint_y=None, height=40)
+        btn_row = MDBoxLayout(orientation="horizontal", spacing="5dp", size_hint_y=None, height=40)
         for route, minutes, color in routes:
             btn = RouteButton(route, minutes, color)
             btn_row.add_widget(btn)
@@ -48,12 +55,24 @@ class StopCard(MDCard):
 
 
 class AllStopsScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.scroll = MDScrollView()
+        self.stops_box = MDBoxLayout(
+            orientation="vertical",
+            padding="10dp",
+            spacing="10dp",
+            size_hint_y=None
+        )
+        self.stops_box.bind(minimum_height=self.stops_box.setter('height'))
+        self.scroll.add_widget(self.stops_box)
+        self.add_widget(self.scroll)
+
     def on_enter(self):
         Clock.schedule_once(lambda dt: self.update_stops())
 
     def update_stops(self):
-        stops_box = self.ids.stops_box
-        stops_box.clear_widgets()
+        self.stops_box.clear_widgets()
 
         # Тестовые данные
         stops_data = [
@@ -63,31 +82,95 @@ class AllStopsScreen(Screen):
 
         for stop_name, routes in stops_data:
             card = StopCard(stop_name, routes)
-            stops_box.add_widget(card)
+            self.stops_box.add_widget(card)
 
 
 class FavoritesScreen(Screen):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.scroll = MDScrollView()
+        self.favorites_box = MDBoxLayout(
+            orientation="vertical",
+            padding="10dp",
+            spacing="10dp",
+            size_hint_y=None
+        )
+        self.favorites_box.bind(minimum_height=self.favorites_box.setter('height'))
+        self.scroll.add_widget(self.favorites_box)
+        self.add_widget(self.scroll)
+
     def on_enter(self):
         Clock.schedule_once(lambda dt: self.update_favorites())
 
     def update_favorites(self):
-        favorites_box = self.ids.favorites_box
-        favorites_box.clear_widgets()
-
-        favorites_box.add_widget(MDLabel(
+        self.favorites_box.clear_widgets()
+        self.favorites_box.add_widget(MDLabel(
             text="Здесь будут избранные остановки",
             halign="center"
         ))
 
 
 class AboutScreen(Screen):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.add_widget(MDLabel(
+            text="Расписание автобусов\nРазработчик: Вы",
+            halign="center"
+        ))
 
 
 class BusApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Light"
-        return Builder.load_file("main.kv")
+        self.theme_cls.primary_palette = "Blue"
+
+        # Создаем ScreenManager
+        screen_manager = ScreenManager()
+
+        # Создаем экраны
+        stops_screen = AllStopsScreen(name="stops")
+        favorites_screen = FavoritesScreen(name="favorites")
+        about_screen = AboutScreen(name="about")
+
+        # Добавляем экраны в менеджер
+        screen_manager.add_widget(favorites_screen)
+        screen_manager.add_widget(stops_screen)
+        screen_manager.add_widget(about_screen)
+
+        # Создаем основной layout
+        main_layout = MDBoxLayout(orientation="vertical")
+        main_layout.add_widget(screen_manager)
+
+        # Создаем нижнюю панель навигации с иконками
+        nav_bar = MDBoxLayout(
+            size_hint_y=None,
+            height="56dp",
+            md_bg_color=(0.95, 0.95, 0.95, 1),
+            spacing="5dp",
+            padding="5dp"
+        )
+
+        # Кнопки навигации с иконками
+        favorites_btn = MDIconButton(
+            icon="star",
+            on_release=lambda x: setattr(screen_manager, 'current', 'favorites')
+        )
+        stops_btn = MDIconButton(
+            icon="bus",
+            on_release=lambda x: setattr(screen_manager, 'current', 'stops')
+        )
+        about_btn = MDIconButton(
+            icon="information",
+            on_release=lambda x: setattr(screen_manager, 'current', 'about')
+        )
+
+        nav_bar.add_widget(favorites_btn)
+        nav_bar.add_widget(stops_btn)
+        nav_bar.add_widget(about_btn)
+
+        main_layout.add_widget(nav_bar)
+
+        return main_layout
 
 
 if __name__ == "__main__":
